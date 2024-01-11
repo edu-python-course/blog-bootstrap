7e7e7a777044632a1ebf5fc59333cc7536996e60
Serhii Horodilov <sgorodil@gmail.com>
2024-01-11 19:30:50 +0200

    Apply webpack development configuration
    - fixes GH-35
    - fixes GH-39
    - GH-38

---
+++ src/index.js
@@ -0,0 +1,8 @@
+// IDE: do not report unused imports
+// noinspection ES6UnusedImports
+
+// Bind sources
+import "./scss/bs-styles.scss"
+
+// Import all of Bootstrap's JS
+import * as bootstrap from "bootstrap"
---
+++ webpack.config.js
@@ -0,0 +1,52 @@
+/**
+ * Webpack configuration
+ *
+ * This configuration uses the default entry point for webpack v5 at
+ * "src/index.js" (omitted inside the config object
+ *
+ */
+
+"use strict"
+
+const path = require("path")
+const autoprefixer = require("autoprefixer")
+const HTMLWebpackPlugin = require("html-webpack-plugin")
+const miniCssExportPlugin = require("mini-css-extract-plugin")
+
+// webpack config object
+module.exports = {
+    mode: "development",
+    output: {
+        filename: "main.bundle.js",
+        path: path.resolve(__dirname, "dist"),
+        clean: true
+    },
+    devServer: {
+      static: path.resolve(__dirname, "dist"),
+      port: 8000,
+      hot: true
+    },
+    plugins: [
+        new miniCssExportPlugin({"filename": "main.min.css"}),
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.scss$/,
+                use: [
+                    {loader: miniCssExportPlugin.loader},
+                    {loader: "css-loader"},
+                    {
+                        loader: "postcss-loader",
+                        options: {
+                            postcssOptions: {
+                                plugins: [autoprefixer]
+                            }
+                        }
+                    },
+                    {loader: "sass-loader"}
+                ]
+            },
+        ]
+    }
+}
