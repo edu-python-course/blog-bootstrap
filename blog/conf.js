b36fc250e461acccc1553f48b3efc3ed5a14800b
Serhii Horodilov <sgorodil@gmail.com>
2024-01-12 06:19:27 +0200

    Adjust topics nav (fixes GH-45)

--- blog/conf.js
+++ blog/conf.js
@@ -7,6 +7,16 @@ const templateParameters = {
     favicon_png: "./img/favicon.png",
     anonymous_img: "./img/anonymous.svg",
     logo: "./img/logo.svg",
+
+    navs: [
+        {link: "?query=fake-latin", title: "Fake Latin"},
+        {link: "?query=esoteric-wisdom", title: "Esoteric Wisdom"},
+        {link: "?query=culinary-inspirations", title: "Culinary Inspirations"},
+        {link: "?query=pirate-lingo", title: "Pirate Lingo"},
+        {link: "?query=science-fiction", title: "Science Fiction"},
+    ],
+
+    BlogSiteTitle: "Blog Templates",
 }
 
 module.exports = {
