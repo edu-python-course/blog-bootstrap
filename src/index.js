6c7c4f2f6ca1914f48ef79af556ef8047b5f13ab
Serhii Horodilov <sgorodil@gmail.com>
2024-01-14 13:55:49 +0200

    Add article form sources (fixes GH-47)

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 // noinspection ES6UnusedImports: do not report unused imports (IDE)
 
-// bind bootstrap sources
 import "bootstrap/scss/bootstrap.scss"
 import * as bootstrap from "bootstrap"
 
-// Bind sources
+import "./js/sidebar"
+
 import "./scss/auth.scss"
 import "./scss/default.scss"
 import "./scss/header.scss"
