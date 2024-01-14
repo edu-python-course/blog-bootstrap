da0b1f3afea1880754ab769ab984ee775e82b2b3
Serhii Horodilov <sgorodil@gmail.com>
2024-01-14 17:49:10 +0200

    Add pagination section to list view (fixes GH-50)

--- tests/list_view_test.py
+++ tests/list_view_test.py
@@ -60,16 +60,43 @@ class TestListView(unittest.TestCase):
         self.assertIsNotNone(element)
         self.assertIn("col", element.get_attribute("class"))
 
+    def test_sections(self):
+        elements = self.browser.find_elements(By.TAG_NAME, "section")
+        self.assertEqual(len(elements), 2)
+        articles, pagination = elements
+        self.assertEqual("ArticlesList", articles.get_attribute("aria-label"))
+        self.assertIn("row", articles.get_attribute("class"))
+        self.assertIn("row-cols-1", articles.get_attribute("class"))
+        self.assertEqual("Pagination", pagination.get_attribute("aria-label"))
+        self.assertIn("row", pagination.get_attribute("class"))
+        self.assertIn("row-cols-1", pagination.get_attribute("class"))
+
     def test_masonry_applied(self):
-        element = self.browser.find_element(By.ID, "articlesContainer")
-        section = element.find_element(By.TAG_NAME, "section")
-        self.assertIsNotNone(section)
-        masonry = json.loads(section.get_attribute("data-masonry"))
+        selector = "#articlesContainer section:first-of-type"
+        element = self.browser.find_element(By.CSS_SELECTOR, selector)
+        masonry = json.loads(element.get_attribute("data-masonry"))
         self.assertDictEqual(masonry, {"percentPosition": True})
 
-    def test_articles_count(self):
-        articles = self.browser.find_elements(By.TAG_NAME, "article")
+    def test_articles(self):
+        selector = "#articlesContainer section:first-of-type"
+        container = self.browser.find_element(By.CSS_SELECTOR, selector)
+        articles = container.find_elements(By.TAG_NAME, "article")
         self.assertEqual(len(articles), 8)
+
+    def test_pagination(self):
+        selector = "#articlesContainer section:last-of-type"
+        container = self.browser.find_element(By.CSS_SELECTOR, selector)
+
+        nav = container.find_element(By.TAG_NAME, "nav")
+        self.assertIn("justify-content-center", nav.get_attribute("class"))
+
+        ul = nav.find_element(By.TAG_NAME, "ul")
+        self.assertIn("pagination", ul.get_attribute("class"))
+
+        for li in ul.find_elements(By.TAG_NAME, "li"):
+            self.assertIn("page-item", li.get_attribute("class"))
+            a = li.find_element(By.TAG_NAME, "a")
+            self.assertIn("page-link", a.get_attribute("class"))
 
 
 if __name__ == "__main__":
